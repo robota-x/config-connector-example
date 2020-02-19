028437c6e6f19db65272b7fb246dfac3d1efd29d
Lorenzo Turrino <lorenzo.turrino@oddschecker.com>
2020-02-19 09:56:13 +0000

    fix bucket upload

--- demo-2/deployments/processor/processor.py
+++ demo-2/deployments/processor/processor.py
@@ -1,27 +1,32 @@
-from datetime import datetime 
+from datetime import datetime
 from google.cloud import pubsub_v1, storage
 from uuid import uuid4
 
 subscriber_client = pubsub_v1.SubscriberClient()
-subscription_path = subscriber_client.subscription_path("lorenzo-scratch-space", "processor")
+subscription_path = subscriber_client.subscription_path(
+    "lorenzo-scratch-space", "processor"
+)
 
 storage_client = storage.Client()
-storage_client.bucket("cc-demo-lorenzo-feedback")
+bucket = storage_client.bucket("cc-demo-lorenzo-feedback")
 
 
 def process_message(message):
-    print(f"Received message: {message}")
+    msg_content = message.data
+    print(f"Received message: {msg_content}")
 
     timestamp = datetime.utcnow().isoformat()
     random_append = uuid4().hex
 
     blob = bucket.blob(f"{timestamp}-{random_append}")
-    blob.upload_from_string(message)
+    blob.upload_from_string(msg_content)
 
     message.ack()
 
 
-streaming_pull_future = subscriber_client.subscribe(subscription_path, callback=process_message)
+streaming_pull_future = subscriber_client.subscribe(
+    subscription_path, callback=process_message
+)
 
 print(f"Listening for messages on {subscription_path}..")
 
